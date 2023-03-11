dd72bec963f7674d3581b2e8118d8fbde083e5b1
reymisterii <reymisterii@gmail.com>
2023-03-11 14:31:46 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
