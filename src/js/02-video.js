65c27826a3d5fefd74b1e9935867026ea110dede
reymisterii <reymisterii@gmail.com>
2023-03-13 23:07:09 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,47 @@
-import Player from '@vimeo/player';
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
-const iframe = document.querySelector('vimeo-player');
-    const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(`vimeo-player`);
+    
+const player = LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const onPlay = function (time) {
+    localStorage.setItem(LOCALSTORAGE_KEY, time.seconds);
+};
+player.on(`timeupdate`, throttle((onPlay), 1000));
 
+player.on(`timeupdate`, onPlay);
+
+console.log(player);
+
+// player.setCurrentTime(30.456).then(function (seconds) {
+    
+// }).catch(function (error) {
+//     switch (error.name) {
+//         case `RangeError`:
+
+//             break;
+
+//         default:
+            
+//             break;
+        
+//     }
+// });
+
+
+
+
+
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+
+// console.log(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
+const STORAGE_KEY = `feedback-form-state`;
+
+const refs = {
+   form: document.querySelector(`.feedback-form`),
+   textarea: document.querySelector(`.feedback-form textarea`),
+};
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit (e) {
+   e.preventDefault();
+   localStorage.removeItem(`STORAGE_KEY`);
+   refs.form.reset();
+   console.log(`Отправляeм форму`); 
+};
+
+function onTextareaInput (e) {
+   const massage = e.target.value;
+   localStorage.setItem(`STORAGE_KEY`, massage);
+};
+
+function populateTextarea() {
+   const savedMessage = localStorage.getItem(`STORAGE_KEY`);
+   if (savedMessage) {
+      console.log(savedMessage);
+
+      refs.textarea.value = savedMessage;
+   }
+};
+
+
 
